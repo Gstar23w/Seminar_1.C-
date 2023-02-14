3e7fd5d08e16c858dc5680772bc64d38922e5e98
Aleksandr <mainvillain98@gmail.com>
2023-02-14 19:51:03 +0300

    added tasks

---
+++ Task07/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int lastNumber = number % 10;
+    Console.WriteLine($"Последняя цифра числа - {lastNumber}");
+}
+else
+  Console.WriteLine("Вы ввели не трехначное число");
