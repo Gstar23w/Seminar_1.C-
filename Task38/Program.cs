f805d51abe61454f021de656ff50c719dc55ae58
Aleksandr <mainvillain98@gmail.com>
2023-03-02 18:56:26 +0300

    added tasks

---
+++ Task38/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray(int[] arr)
+{
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+else Console.Write($"{arr[i]}");
+}
+Console.Write("]");
+}
+
+int GetMaxInArray (int[] arr)
+{
+    int max = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(max <= arr[i]) 
+        max = arr[i];
+    }
+    
+    return (max);
+}
+
+int GetMinInArray (int[] arr)
+{
+    int min = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(min >= arr[i]) 
+        min = arr[i];
+    }
+
+    return (min);
+}
+
+int [] arr = CreateArrayRndInt(5, 0, 20);
+PrintArray(arr);
+GetMaxInArray(arr);
+GetMinInArray(arr);
+int sum = GetMaxInArray(arr) - GetMinInArray(arr);
+Console.WriteLine($"->{sum}");
